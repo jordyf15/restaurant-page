bfc527a7dbea13d2fc42b1f87948271d8539349b
jordyf15 <jordyferdian88@gmail.com>
2021-10-17 12:50:56 +0700

    import all image asset for menu tab in menu.js

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,13 @@
+import ale from '../assets/images/ale.png';
+import beefAndShrooms from '../assets/images/beef-and-shrooms.png';
+import breadAndStew from '../assets/images/bread-and-stew.png';
+import cooper from '../assets/images/copper.png';
+import gold from '../assets/images/gold.png';
+import grilledFish from '../assets/images/grilled-fish.png';
+import mead from '../assets/images/mead.png';
+import silver from '../assets/images/silver.png';
+import skewersAndCord from '../assets/images/skewers-and-corn.png';
+
 class MenuItem{
     image;
     name;
