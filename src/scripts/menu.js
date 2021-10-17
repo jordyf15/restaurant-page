1076e2ed36371cd34dea101a23e4eb9a149d0951
jordyf15 <jordyferdian88@gmail.com>
2021-10-17 13:03:40 +0700

    create menuList

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -6,7 +6,7 @@ import gold from '../assets/images/gold.png';
 import grilledFish from '../assets/images/grilled-fish.png';
 import mead from '../assets/images/mead.png';
 import silver from '../assets/images/silver.png';
-import skewersAndCord from '../assets/images/skewers-and-corn.png';
+import skewersAndCorn from '../assets/images/skewers-and-corn.png';
 
 class MenuItem{
     image;
@@ -22,6 +22,15 @@ class MenuItem{
         this.copper = copper;
     }
 }
+
+const menuList = [
+    new MenuItem(beefAndShrooms, 'Beef and Shrooms', 0, 4, 18),
+    new MenuItem(grilledFish, 'Grilled Fish', 0, 2, 7),
+    new MenuItem(breadAndStew, 'Bread and Stew', 0, 0, 80),
+    new MenuItem(skewersAndCorn, 'Skewers and Corn', 0, 1, 47),
+    new MenuItem(ale, 'Ale', 0, 0, 30),
+    new MenuItem(mead, 'Mead', 0, 0, 40)
+]
 
 function renderMenu(){
     const contentContainer = document.querySelector('#content');
