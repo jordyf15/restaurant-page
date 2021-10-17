14f5b80027d65b2f86df851b2fe04b610f9e8a3f
jordyf15 <jordyferdian88@gmail.com>
2021-10-17 12:45:29 +0700

    create popup message in home tab

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -26,46 +26,67 @@ function renderHome(){
 }
 
 function enterTavernPopup() {
-    const homeContainer = document.querySelector('#home-container');
+    const body = document.querySelector('body');
     const popUpBackground=document.createElement('div');
     popUpBackground.id = 'popup-background';
-    homeContainer.appendChild(popUpBackground);
+    popUpBackground.addEventListener('click', closePopUp);
+    body.appendChild(popUpBackground);
 
     const popUpContainer = document.createElement('div');
     popUpContainer.id = 'popup-container';
-    homeContainer.appendChild(popUpContainer);
+    popUpContainer.addEventListener('click', (e)=>{
+        e.stopPropagation();
+    });
+    popUpBackground.appendChild(popUpContainer);
 
     const popUpText = document.createElement('p');
     popUpText.id = 'popup-text';
-    popUpText.textContent = 'As you enter the tavern on the cold windy night, the cold dissipates from your body as you can feel the warmth of the hearth, the laughters and chatter in the tavern that seem to swept your worries, and the smell of food that reminds you how long have you travelled tirelessly as you sit yourself somewhere in the tavern.'
-    +'"Well, what are you waiting lad? Go order something" the tavern keeper mond said.';
+    popUpText.textContent = `As you enter the tavern on the cold windy night, the cold dissipates from your body as you feel the warmth of the hearth, 
+    the laughters and chatter in the tavern that seem to swept away your worries, and the smell of food that reminds you of how long you have
+    travelled tirelessly as you sit yourself somewhere in the tavern.`;
+    popUpContainer.appendChild(popUpText);
 
-
-    homeContainer.appendChild(popUpText);
+    const popUpQuote = document.createElement('p');
+    popUpQuote.textContent = '"Well, what are you waiting lad? Go order something" the tavern keeper mond said.';
+    popUpContainer.appendChild(popUpQuote);
 
     const closePopupButton = document.createElement('button');
     closePopupButton.textContent = 'Done';
+    closePopupButton.addEventListener('click', closePopUp);
+    popUpContainer.appendChild(closePopupButton);
 }
 
 function continueJourneyPopup(){
-    const homeContainer = document.querySelector('#home-container');
+    const body = document.querySelector('body');
     const popUpBackground=document.createElement('div');
     popUpBackground.id = 'popup-background';
-    homeContainer.appendChild(popUpBackground);
+    popUpBackground.addEventListener('click', closePopUp);
+    body.appendChild(popUpBackground);
 
     const popUpContainer = document.createElement('div');
     popUpContainer.id = 'popup-container';
-    homeContainer.appendChild(popUpContainer);
+    popUpContainer.addEventListener('click', (e)=>{
+        e.stopPropagation();
+    });
+    popUpBackground.appendChild(popUpContainer);
 
     const popUpText = document.createElement('p');
     popUpText.id = 'popup-text';
-    popUpText.textContent = 'You see folks going in and out of that tavern, but not you. You have business elsewhere. ' +
-    'So you decided to continue your journey down the road as the noice from the tavern grows smaller and the road ahead of you seems darken. ' +
-    'But that does not matter, you have business elsewhere and time is of the essence.'
-    homeContainer.appendChild(popUpText);
+    popUpText.textContent = `You see folks going in and out of that tavern, but not you. You have business elsewhere. 
+    So you decided to continue your journey down the road as the noice from the tavern grows smaller and the road ahead of you seems darken. 
+    But that does not matter, you have business elsewhere and time is of the essence.`
+    popUpContainer.appendChild(popUpText);
 
     const closePopupButton = document.createElement('button');
     closePopupButton.textContent = 'Done';
+    closePopupButton.addEventListener('click', closePopUp);
+    popUpContainer.appendChild(closePopupButton);
+}
+
+function closePopUp(){
+    const body = document.querySelector('body');
+    const popUpBackground = document.querySelector('#popup-background');
+    body.removeChild(popUpBackground);
 }
 
 export {renderHome}
